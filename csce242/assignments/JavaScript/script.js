39f56d2a250b6ec6fd20ba5cfd8c9309300eefd1
What521 <rubenkgeorge@gmail.com>
2025-02-09 15:40:59 -0500

    commitment

--- csce242/assignments/JavaScript/script.js
+++ csce242/assignments/JavaScript/script.js
@@ -3,7 +3,14 @@ document.getElementById("hello-printer").onclick = () =>{
         document.getElementById("result").innerHTML += "hello<br>"; 
 };
 
-document.getElementById("colorful-btn").onclick = () => {
-    const messageP = document.getElementById("star");
-}
+const svgEl = document.querySelector("svg"); 
+colorChanger.addEventListener("input", function() {
+    svgEl.setAttribute("fill", colorChanger.value);
+});
+
+document.getElementById("image1").onclick = () => {
+    document.getElementById("image1").src="images/random2.jfif"; 
+};
+
+
 
