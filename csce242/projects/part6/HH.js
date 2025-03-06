a1330a883825d6626d6234a2c05905d6766b88d0
What521 <rubenkgeorge@gmail.com>
2025-03-05 23:41:13 -0500

    jsON

--- csce242/projects/part6/HH.js
+++ csce242/projects/part6/HH.js
@@ -2,3 +2,37 @@ document.getElementById("toggle-bar").onclick = () =>{
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const getMovieDetails = async () =>{
+    const link = "https://what521.github.io/csce242/projects/part6/movieDetails.json";
+        try {
+            const response = await fetch(link);
+            return response.json();
+        } catch(error){
+            console.log(error);
+        }
+};
+
+const showMovieDetails = async () =>
+{
+    const MovieDetails = await getMovieDetails ();
+
+    MovieDetails.forEach = ((Detail)=>{
+        const recUl = document.getElementById("recommended");
+        const li = document.createElement("li");
+        const section = document.createElement("section");
+        li.append(section);
+        recUl.append(li);
+
+        const h1 = document.createElement("h1");
+        h1.innerHTML = Detail.titleR;
+        section.append(h1);
+
+        const image = document.createElement("img");
+        image.src = Detail.imagesR;
+        section.append(image);
+
+        const p1 = document.createElement("p");
+        p1.innerHTML = Detail.
+        
+    })
+}
